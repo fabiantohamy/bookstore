0b00414cfb7cc9796b8080c64eda5f0e1443d42f
fabiantohamy <fabian.tohamy@hotmail.com>
2025-02-02 23:25:22 +0200

    update

--- src/main/java/fi/haagahelia/course/web/BookController.java
+++ src/main/java/fi/haagahelia/course/web/BookController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import fi.haagahelia.course.domain.Student;
 import fi.haagahelia.course.domain.StudentRepository;
+import fi.haagahelia.course.domain.Book;
 import fi.haagahelia.course.domain.BookRepository;
 
 @Controller
@@ -22,6 +23,24 @@ public class BookController {
     public String bookList(Model model) {
         model.addAttribute("books", repository.findAll());
         return "booklist";
+    }
+
+    @RequestMapping(value = "/addbook", method = RequestMethod.GET)
+    public String addBookForm(Model model) {
+        model.addAttribute("book", new Book());
+        return "addbook";
+    }
+
+    @RequestMapping(value = "/addbook", method = RequestMethod.POST)
+    public String addBook(Book book) {
+        repository.save(book);
+        return "redirect:/booklist";
+    }
+
+    @RequestMapping(value = "/deletebook/{id}", method = RequestMethod.GET)
+    public String deleteBook(@PathVariable("id") Long bookId) {
+        repository.deleteById(bookId);
+        return "redirect:/booklist";
     }
 	
     @RequestMapping(value= {"/", "/studentlist"})
